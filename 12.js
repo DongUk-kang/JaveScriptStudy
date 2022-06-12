2f7442903f0a4bb848543c433c939b1890c21e99
Dong Uk Kang <gusrn1255@naver.com>
2022-06-12 11:23:54 +0900

    # 12 - 10

--- 12.js
+++ 12.js
@@ -426,3 +426,16 @@ function add(x, y) {
     return x + y;
 }
 console.log(add(2, 5, 10));
+
+// 12 - 20
+function add(x, y) {
+    console.log(arguments);
+    return x + y;
+}
+
+add(2, 5, 10);
+
+/**
+ * arguments 객체는 함수를 정의할때 매겨변수를 개수를 확정할 수 없는 가변 인자 함수를 구현할때
+ * 유용하게 사용됨
+ */

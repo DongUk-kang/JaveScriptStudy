6835f84e12c55af74e8fd19cdddf772700ab96d7
Dong Uk Kang <gusrn1255@naver.com>
2022-07-09 23:02:55 +0900

    # 17 - 6

--- 17.js
+++ 17.js
@@ -245,3 +245,39 @@ function Circle(radius) {
  * 즉, this에 바인딩되어 있는 인스턴스에 프로퍼티나 메서드를 추가하고 생성자 함수가 인수로 전달 받은 초기값을 인스턴스 프로퍼티에 할당하여
  * 초기화 하거나 고정값을 할당함
  */
+
+
+// 17 - 09
+function Circle(radius) {
+    //1. 암묵적으로 인스턴스가 생성되고 this에 바인딩됨
+
+    //2. this에 바인딩되어 잇는 인스턴스를 초기화됨
+    this.radius = radius;
+    this.getDiameter = function () {
+        return 2 * this.radius;
+    };
+}
+
+/**
+ * 3. 인스턴스 반환
+ * 생성자 함수 내부의 모든 처리가 끝나면 완성된 인스턴스가 바인딩된 this가 암묵적으로 반환
+ */
+
+// 17 - 10
+function Circle(radius) {
+     //1. 암묵적으로 인스턴스가 생성되고 this에 바인딩됨
+
+    //2. this에 바인딩되어 잇는 인스턴스를 초기화됨
+    this.radius = radius;
+    this.getDiameter = function () {
+        return 2 * this.radius;
+    };
+
+    // 3. 완성된 인스턴스가 바인됭 this가 암묵적으로 반환됨
+}
+
+// 인스턴스 생성. Circle 생성자 함수는 암묵적으로 this를 반환함
+const circle7 = new Circle(1);
+console.log(circle7);
+
+//만약 this가 아닌 객체를 명시적으로 반환하면 this가 반환되지 못하고 return문에 명시한 객체가 반환됨

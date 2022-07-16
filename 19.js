242e9988f72e103a06cb507f60376be8d8113be2
Dong Uk Kang <gusrn1255@naver.com>
2022-07-16 23:08:55 +0900

    # 19 - 2

--- 19.js
+++ 19.js
@@ -91,3 +91,61 @@ console.log(circle.getArea());
  * 중복을 제거하는 방법은 기존의 코드를 적극적으로 재사용할 수 있음
  * 코드 재사용은 개발비용을 현저히 주일 수 있는 잠재력이 있으므로 매우중요함
  */
+
+// 19 - 03
+//생성자 함수
+function circle(radius) {
+  this.radius = radius;
+  this.getArea = function () {
+    //Math.PI는 원주율을 나태내는 상수다.
+    return Math.PI * this.radius ** 2;
+  };
+}
+
+// 반지름이 1인 인스턴스 생성
+const circle1 = new circle(1);
+// 반지름이 2인 인스턴스 생성
+const circle2 = new circle(2);
+
+/**
+ * Circle 생성자 함수는 인스턴스를 생성할 때마다 동일한 동작을 하는
+ * getArea 메서드를 중복생성하고 모든 인스턴스가 중복 소유한다.
+ * getArea 메서드는 하나만 생성하여 모든 인스턴스가 공유해서 사용하는 것이 바람직
+ */
+console.log(circle.getArea === circle2.getArea); // false
+console.log(circle1.getArea());
+console.log(circle2.getArea());
+
+/**
+ * 생성자 함수에 의해 생성된 모든 인스턴스가 동일한 메서드를 중복 소유하는 것은 메모리를 불필요하게 낭비함
+ * 또한 인스턴스를 생성할때마다 메서드를 생성하므로 퍼포먼스에도 악영향을 끼침
+ *
+ * 만약 10개의 인스턴스를 생성하면 내용이 동일한 메서드도 10개 생성된다.
+ */
+
+// 19 - 04
+//생성자 함수
+function Circle(radius) {
+  this.radius = radius;
+}
+/**
+ * Circle 생성자 함수가 생성한 모든 인스턴스가 getArea 메서드를
+ * 공유해서 사용할 수 있도록 프로토타입에 추가함
+ * 프로토타입은 Circle 생성자의 prototype 프로퍼티에 바인되어있음
+ */
+Circle.prototype.getArea = function () {
+  return Math.PI * this.radius ** 2;
+};
+
+// 인스턴스 생성
+const circle3 = new Circle(3);
+const circle4 = new Circle(4);
+
+/**
+ * Circle 생성자 함수가 생성한 모든 인스턴스는 부모 객체의 역할을 하는
+ * 프로토타입 Circle.prototype으로부터 getArea 메서드를 상속받음
+ * 즉, Circle 생성자 함수가 생성하는 모든 인스턴스는 하나의 getArea 메서드를 공유함
+ */
+console.log(circle3.getArea === circle4.getArea); // true
+console.log(circle3.getArea());
+console.log(circle4.getArea());

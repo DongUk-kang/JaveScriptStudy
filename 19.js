793a851287154751ac03904f2a07ca9787c43643
Dong Uk Kang <gusrn1255@naver.com>
2022-07-17 00:24:14 +0900

    # 19 - 6

--- 19.js
+++ 19.js
@@ -298,3 +298,23 @@ function Person(name) {
 const me1 = new Person("Leee");
 //me1 객체의 생성자 함수는 Person
 console.log(me1.constructor === Person);
+
+// 19.4 리터럴 표기법에의해 생성된 객체의 생성자 함수와 프로토타입
+
+// 19 - 15
+
+//obj 객체를 생성한 생성자 함수는 Object다
+const obj4 = new Object();
+console.log(obj4.constructor === Object); // true
+
+//add 함수 객체를 생성한 생성자 함수는 Function이다
+const add = new Function("a", "b", "return a + b");
+console.log(obj.constructor == Object); // true
+
+//생성자 함수
+function Person(name) {
+  this.name = name;
+}
+//me 객체를 생성한 생성자함수는 Person이다.
+const me2 = new Person("Kiem");
+console.log(me.constructor === Person); // true
